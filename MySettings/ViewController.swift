d42361f2829751df084c63f895d2b6f096d28ee0
cis290 <cis290@PC22766.local>
2016-10-28 15:45:37 -0500

    cs

---
+++ MySettings/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MySettings
+//
+//  Created by cis290 on 10/28/16.
+//  Copyright © 2016 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var fullname: UITextField!
+    
+    @IBOutlet weak var address: UITextField!
+    
+    @IBAction func load(sender: UIButton) {
+    }
+    
+    @IBAction func save(sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
